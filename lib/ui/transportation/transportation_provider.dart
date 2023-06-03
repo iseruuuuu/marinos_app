f12517a75c926594dd5fa5513c73cf84c850241b
iseruuuuu <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>
2023-06-03 22:35:02 +0900

    add: 読み込みのLoadingを追加した

--- lib/ui/transportation/transportation_provider.dart
+++ lib/ui/transportation/transportation_provider.dart
@@ -1,6 +1,17 @@
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final transportationProvider = StateProvider<InAppWebViewController?>(
-  (ref) => null,
+final transportationProvider =
+    StateProvider<InAppWebViewController?>((_) => null);
+
+final loadingProvider = StateNotifierProvider<LoadingNotifier, bool>(
+  (_) => LoadingNotifier(),
 );
+
+class LoadingNotifier extends StateNotifier<bool> {
+  LoadingNotifier() : super(false);
+
+  void setLoading(bool isLoading) {
+    state = isLoading;
+  }
+}
--- lib/ui/transportation/transportation_screen.dart
+++ lib/ui/transportation/transportation_screen.dart
@@ -10,85 +10,110 @@ class TransportationScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final transportation = ref.watch(transportationProvider.notifier);
+    final isLoading = ref.watch(loadingProvider);
     return SafeArea(
       top: false,
       child: Scaffold(
         appBar: AppBar(
           elevation: 0,
         ),
-        body: Column(
+        body: Stack(
           children: [
-            Expanded(
-              child: InAppWebView(
-                onWebViewCreated: (controller) {
-                  transportation.state = controller;
-                },
-                initialUrlRequest:
-                    URLRequest(url: Uri.parse('https://transit.yahoo.co.jp/')),
-                initialOptions: InAppWebViewGroupOptions(
-                  crossPlatform: InAppWebViewOptions(
-                    transparentBackground: true,
-                    useShouldOverrideUrlLoading: true,
-                    clearCache: true,
-                    cacheEnabled: false,
-                  ),
-                ),
-                shouldOverrideUrlLoading: (controller, navigationAction) async {
-                  var uri = navigationAction.request.url!;
-                  if (![
-                    "http",
-                    "https",
-                    "file",
-                    "chrome",
-                    "data",
-                    "javascript",
-                    "about"
-                  ].contains(uri.scheme)) {
-                    if (await canLaunchUrl(uri)) {
-                      await launchUrl(
-                        uri,
-                      );
-                      return NavigationActionPolicy.CANCEL;
-                    }
-                  }
-                  return NavigationActionPolicy.ALLOW;
-                },
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                border: Border(
-                  top: BorderSide(width: 0, color: Colors.black),
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    onPressed: () => transportation.state!.goBack(),
-                    icon: const Icon(Icons.arrow_back_ios_new),
-                  ),
-                  IconButton(
-                    onPressed: () => transportation.state!.goForward(),
-                    icon: const Icon(Icons.arrow_forward_ios),
-                  ),
-                  IconButton(
-                    onPressed: () => transportation.state!.reload(),
-                    icon: const Icon(Icons.refresh),
-                  ),
-                  IconButton(
-                    onPressed: () => transportation.state!.loadUrl(
-                      urlRequest: URLRequest(
-                        url: Uri.parse('https://transit.yahoo.co.jp/'),
+            Column(
+              children: [
+                Expanded(
+                  child: InAppWebView(
+                    onWebViewCreated: (controller) {
+                      transportation.state = controller;
+                    },
+                    onLoadStart: (controller, url) {
+                      ref.read(loadingProvider.notifier).setLoading(true);
+                    },
+                    onLoadStop: (controller, url) {
+                      ref.read(loadingProvider.notifier).setLoading(false);
+                    },
+                    initialUrlRequest: URLRequest(
+                      url: Uri.parse(
+                        'https://transit.yahoo.co.jp/',
                       ),
                     ),
-                    icon: const Icon(Icons.home),
+                    initialOptions: InAppWebViewGroupOptions(
+                      crossPlatform: InAppWebViewOptions(
+                        transparentBackground: true,
+                        useShouldOverrideUrlLoading: true,
+                        clearCache: true,
+                        cacheEnabled: false,
+                      ),
+                    ),
+                    shouldOverrideUrlLoading:
+                        (controller, navigationAction) async {
+                      var uri = navigationAction.request.url!;
+                      if (![
+                        "http",
+                        "https",
+                        "file",
+                        "chrome",
+                        "data",
+                        "javascript",
+                        "about"
+                      ].contains(uri.scheme)) {
+                        if (await canLaunchUrl(uri)) {
+                          await launchUrl(
+                            uri,
+                          );
+                          return NavigationActionPolicy.CANCEL;
+                        }
+                      }
+                      return NavigationActionPolicy.ALLOW;
+                    },
                   ),
-                ],
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    border: Border(
+                      top: BorderSide(width: 0, color: Colors.black),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        onPressed: () => transportation.state!.goBack(),
+                        icon: const Icon(Icons.arrow_back_ios_new),
+                      ),
+                      IconButton(
+                        onPressed: () => transportation.state!.goForward(),
+                        icon: const Icon(Icons.arrow_forward_ios),
+                      ),
+                      IconButton(
+                        onPressed: () => transportation.state!.reload(),
+                        icon: const Icon(Icons.refresh),
+                      ),
+                      IconButton(
+                        onPressed: () => transportation.state!.loadUrl(
+                          urlRequest: URLRequest(
+                            url: Uri.parse('https://transit.yahoo.co.jp/'),
+                          ),
+                        ),
+                        icon: const Icon(Icons.home),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            if (isLoading)
+              const Center(
+                child: SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 8,
+                  ),
+                ),
               ),
-            )
           ],
         ),
       ),
