143a0ea616af68995eba111fd88fa3e6f791e484
iseruuuuu <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>
2023-06-04 16:39:01 +0900

    remove file

--- lib/constants/restaurant_constants.dart
+++
@@ -1,4 +0,0 @@
-class RestaurantConstants {
-  static const String restaurantApikey =
-      "kXyTcolnF4AbgYEMH06njdQN7t4Fi1bjZNqfbMCanb-zGO1KFMxtadKtim2m5YrbdgnUvQ3ERaPxWnDSZkiJ2ZnR4qWzS39VMQLFbjy3gno1C-vg-PG_cB0lxdN7ZHYx";
-}
